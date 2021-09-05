8509cd21e97391aba4b1593e417986f6d7e756c6
Gonçalo Silva <goncalosilva1992@gmail.com>
2021-09-05 23:50:31 +0100

    done Unit Test

--- src/test/java/com/springboot/sqlite/service/CustomerServiceTest.java
+++ src/test/java/com/springboot/sqlite/service/CustomerServiceTest.java
@@ -1,22 +1,43 @@
 package com.springboot.sqlite.service;
 
+import com.springboot.sqlite.domain.Customer;
+import com.springboot.sqlite.repository.CustomerRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-//@RunWith(MockitoJUnitRunner.class)
-class CustomerServiceTest {
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerServiceTest {
+
     //Class where you have the methods I'm testing.
-/*    @InjectMocks
-    TeamService teamService;
+    @InjectMocks
+    CustomerService customerService;
 
     //All services external to the class I'm testing.
     @Mock
-    TeamRepository teamRepository;
+    CustomerRepository customerRepository;
 
-    //createTeam
-    @Test*/
+
+    //find all customer
+    @Test
+    public void findAll_thrownAnException(){
+        //Given
+        List<Customer> allCustomerGiven = new ArrayList<>();
+        allCustomerGiven.isEmpty();
+
+        //When
+        Mockito.when(customerRepository.findAll()).thenReturn(allCustomerGiven);
+        Assert.assertThrows(NullPointerException.class, () -> customerService.allCustomer());
+    }
 
 }
